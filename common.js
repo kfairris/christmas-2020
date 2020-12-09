7d74c6f637c9d5b21a4fb3320733d8d572cd0156
Kenny Fairris <kfairris@gmail.com>
2020-12-09 16:22:02 -0600

    clean up old code

--- common.js
+++ common.js
@@ -1,26 +1,3 @@
-// function hamburgerToggle(x) {
-//   x.classList.toggle("change");
-// }
-
-// let dropdown = document.querySelector('.hamburger-main');
-// let items = document.querySelectorAll('.menu_item');
-// const itemsArray = [...items];
-
-// dropdown.addEventListener('click', (e) => {
-//   if (dropdown.classList.contains('closed')) {
-//     dropdown.classList.remove('closed');
-//     itemsArray.forEach(node => node.classList.add('open'));
-//   } else {
-//     dropdown.classList.add('closed');
-//     itemsArray.forEach(node => node.classList.remove('open'));
-//   }
-// });
-
-// itemsArray.addEventListener('click', (s) => {
-//   console.log('clicked item');
-//   dropdown.classList.add('closed');
-//   itemsArray.forEach(node => node.classList.remove('open'));
-// });
 
 // https://stackoverflow.com/questions/64873128/how-to-make-my-burger-menu-dissapear-when-clicking-on-link
 
@@ -51,6 +28,7 @@ const navSlide = () => {
   });
 }
 navSlide();
+
 // Toggling snow off and on
 document.getElementById('snowToggle').onclick = toggleSnow
 
